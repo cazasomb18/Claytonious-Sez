2255f581a46b40268d8fdeb379dd4bbc5b110101
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-04-05 17:05:49 -0500

    html shell created, min CSS formatting, buttons added, event listeners created

---
+++ js/app.js
@@ -0,0 +1,105 @@
+console.log('PROJECT ONE - CLAYTON COMMANDS');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//listeners/handlers
+
+
+document.addEventListener("keydown", (e) => {
+	if(['ArrowUp'].includes(event.key)){
+	console.log('redButton was pressed.')
+	}
+});
+
+document.addEventListener("keydown", (e) => {
+	if(['ArrowRight'].includes(event.key)){
+	console.log('yellowButton was pressed.')	
+	}
+});
+
+document.addEventListener("keydown", (e) => {
+	if(['ArrowDown'].includes(event.key)){
+	console.log('blueButton was pressed.')
+	}
+});
+
+document.addEventListener("keydown", (e) => {
+	if(['ArrowLeft'].includes(event.key)){
+	console.log('greenButton was pressed.')	
+	}
+});
+
+// document.addEventListener('keydown', (event) => {
+//   if(['ArrowDown', 'ArrowUp', 'ArrowRight', 'ArrowLeft'].includes(event.key)) {
+//     cmdrCircle.move(event.key)
+//   }
+
+// $("#redButton").on('keydown', (e) => {
+//     console.log('redButton was clicked');
+
+//      console.log(``);
+// });
+
+// $("#yellowButton").on('keydown', (e) => {
+//     console.log('yellowButton was clicked');
+    
+//     console.log(``);
+// });
+
+// $("#blueButton").on('keydown', (e) => {
+//     console.log('blueButton was clicked');
+
+//     console.log(``);
+// });
+
+// $("#greenButton").on('keydown', (e) => {
+//     console.log('greenButton was clicked');
+
+//      console.log(``);
+// });
+
+
+
+
+
