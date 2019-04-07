98d0a3b603119ceec3f1a3806c3934ed8d494cdc
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-04-07 15:56:46 -0500

    setTimeout() added to userInput() and works

--- js/app.js
+++ js/app.js
@@ -23,9 +23,9 @@ const App = {
 	playGame(){		//game will run off of this fun
 		this.arrOutput();	//output will display
 		//could I insert a function here to have a timer run while the user enters input??
-		
-		this.arrPlayerInput();  //input collected from user here
-		this.winOrLose();		//win/lose() compares arrs and displays msg to console display
+
+		this.playerInput();  //input collected from user here
+		// this.winOrLose();		//win/lose() compares arrs and displays msg to console display
 	},
 	enterName(){
 
@@ -38,21 +38,35 @@ const App = {
 			console.log(arrApp);
 	},
 	playerInput(){		//each time keystrokes are pressed push that value into array.
-		let arrPlayer = App.arrPlayer;
-		// getInput(){} --> user getter here to GET INPUT
-		console.log(App.arrPlayer);	//add timer function that forces uesr to enter strokes in set amount of time... at first...	
+		// let arrPlayer = App.arrPlayer;
+		console.log(App.arrPlayer);	//add timer function that forces user to enter strokes in set amount of time...
 
-	},
-	winOrLose(){
-		if (App.arrOutput === App.playerInput) {  //win/loose logic and messages.
-			console.log(`You managed to do ONE THING RIGHT... I guess...`)
-			App.counter ++;
-		} else {
-			console.log('You had ONE JOB!!! YOU BLEW IT!!!');
+//you need logic here that resets App.arrPlayer everytime a new value is added to App.arrApp.
+		setTimeout( function(){
+			if (this.arrApp === this.arrPlayer){
+				console.log(`You managed to do ONE THING RIGHT... I guess...`)
+				//also display this message to the game console msg center...
+				this.gameCounter ++;
+			} else {
+				console.log(`You have ONE JOB!!! You BLEW IT!!!`);
+				//also display this message to the game console msg center...
+				this.gameCounter === App.gameCounter;
+			}
+		}, 5000/*, p1, p2*/) //timer is working the way intended you have 10 seconds to have the user 
 
-		}
+
 	}
 };
+	// winOrLose(){
+	// 	if (App.arrOutput === App.playerInput) {  //win/loose logic and messages.
+	// 		console.log(`You managed to do ONE THING RIGHT... I guess...`)
+	// 		App.counter ++;
+	// 	} else {
+	// 		console.log('You had ONE JOB!!! You BLEW IT!!!');
+
+	// 	}
+	// }
+/*};*/
 
 
 
@@ -77,7 +91,7 @@ const App = {
 //add events for every key press:  when arrInput receives new keypress light up btn
 
 document.addEventListener("keydown", (e) => {		//need to add animation logic for when keypress occurs here
-	if(['ArrowUp'].includes(event.key)){
+	if(['ArrowUp'].includes(event.key)){			// "ArrowUp" === keydown 38.
 	console.log('redButton was pressed.');
 	App.arrPlayer.push(App.arrColorValues[0]);
 	console.log(App.arrPlayer);
@@ -85,7 +99,7 @@ document.addEventListener("keydown", (e) => {		//need to add animation logic for
 });
 
 document.addEventListener("keydown", (e) => {		//need to add animation logic for when keypress occurs here
-	if(['ArrowRight'].includes(event.key)){
+	if(['ArrowRight'].includes(event.key)){			// "ArrowRight" === keydown 39;
 	console.log('yellowButton was pressed.');
 	App.arrPlayer.push(App.arrColorValues[1]);
 	console.log(App.arrPlayer);
@@ -94,7 +108,7 @@ document.addEventListener("keydown", (e) => {		//need to add animation logic for
 });
 
 document.addEventListener("keydown", (e) => {		//need to add animation logic for when keypress occurs here
-	if(['ArrowDown'].includes(event.key)){
+	if(['ArrowDown'].includes(event.key)){			// "ArrowDown" === keydown 40;
 	console.log('blueButton was pressed.');
 	App.arrPlayer.push(App.arrColorValues[2]);
 	console.log(App.arrPlayer);
@@ -102,7 +116,7 @@ document.addEventListener("keydown", (e) => {		//need to add animation logic for
 });
 
 document.addEventListener("keydown", (e) => {		//need to add animation logic for when keypress occurs here
-	if(['ArrowLeft'].includes(event.key)){
+	if(['ArrowLeft'].includes(event.key)){			// "ArrowLewft" === keydown 37
 	console.log('greenButton was pressed.');
 	App.arrPlayer.push(App.arrColorValues[3]);
 	console.log(App.arrPlayer);
@@ -115,6 +129,8 @@ document.addEventListener("keydown", (e) => {		//need to add animation logic for
 // 	console.log(`${petName} IS ALIVE!! BE RESPONSIBLE!!`);
 // 	App.playGame();
 // });
+
+
 
 
 
