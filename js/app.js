2771ad2106edaef22dc680594f10c9b85690f1c8
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-04-06 22:35:00 -0500

    updating logic / css

--- js/app.js
+++ js/app.js
@@ -1,24 +1,12 @@
 console.log('PROJECT ONE - CLAYTON COMMANDS');
 
-// class pattern {
+// class Game {
 // 	constructor(){
-// 		red = this.red;			//trying to randomize order and instances of
-// 		yellow this.yellow;
-// 		blue = this.blue;
-// 		green = this.green;
+// 		new App.playGame();
 // 	}
 // }
 
-
-
-
-
-
-const colorValues = ["redButton", "yellowButton", "blueButton", "greenButton"];
-randInt = Math.floor(Math.random()*4);
-console.log(randInt);
-console.log(colorValues[randInt]);
-
+// currentGame();
 
 
 
@@ -26,23 +14,30 @@ console.log(colorValues[randInt]);
 
 
 const App = {
-	timerHandle: 0,		//round will end when timer has ended
-	// arrApp: [],
+	timerHandle: 0,	//round will end when timer has ended
+	arrColorValues: ["redButton", "yellowButton", "blueButton", "greenButton"],
+	arrApp: [],
+	arrPlayer: [],
+	currentGame: 0,  //keeps track of the game that is currently in play... or maybe I could use a class constructor to generate a game... hmmm....
+
 	playGame(){		//game will run off of this func
-		this.arrOutput();
+		this.arrOutput();	//output will display
+		// this.playerInput();	//then input will be collected from player
+		// this.winOrLose(); //then winOrLose func will determine whether or not player wins.
+		//
 
 	},
 	arrOutput: function (){			
 		// for (let i = 0; i > 3; i++){
-			let arrApp = [];
-			const arrColorValues = ["redButton", "yellowButton", "blueButton", "greenButton"];		  //jQuery selectors? $""
+			let arrApp = App.arrApp;
+			let arrColorValues = App.arrColorValues;		  //jQuery selectors? $""
 			const randIntZeroAndThree = Math.floor(Math.random()*4);
 			arrApp.push(arrColorValues[randIntZeroAndThree]);
 			console.log(arrApp);
 		// }
 	},
 	playerInput: function (){		//get input from user to be pushed into this array...
-		let arrPlayer = [''];		
+		let arrPlayer = App.arrPlayer;		
 	},
 	winOrLose: function(){
 		if (App.arrOutput === App.playerInput) {  //win/loose logic and messages.
