645542ede6c001c83ba4c629699847ff2c51ac82
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-04-07 13:37:07 -0500

    arrow keys populate arrPlayer userInput() needs more logic

--- js/app.js
+++ js/app.js
@@ -20,47 +20,46 @@ const App = {
 	arrPlayer: [],
 	gameCounter: 0,  //keeps track of the game that is currently in play... or maybe I could use a class constructor to generate a game... hmmm....
 
-	playGame(){		//game will run off of this func
+	playGame(){		//game will run off of this fun
 		this.arrOutput();	//output will display
-		// this.playerInput();	//then input will be collected from player
-		this.winOrLose(); //then winOrLose func will determine whether or not player wins.
-		//
+		//could I insert a function here to have a timer run while the user enters input??
+		this.arrPlayerInput();  //input collected from user here
+		this.winOrLose();		//win/lose() compares arrs and displays msg to console display
+	},
+	enterName(){
 
 	},
-	arrOutput: function (){			
-		// for (let i = 0; i > 3; i++){
+	arrOutput(){
 			let arrApp = App.arrApp;
 			let arrColorValues = App.arrColorValues;		  //jQuery selectors? $""
 			const randIntZeroAndThree = Math.floor(Math.random()*4);
 			arrApp.push(arrColorValues[randIntZeroAndThree]);
 			console.log(arrApp);
-		// }
 	},
-	playerInput: function (){		//get input from user to be pushed into this array...
-		let arrPlayer = App.arrPlayer;		
+	playerInput(){		//each time keystrokes are pressed push that value into array.
+		let arrPlayer = App.arrPlayer;
+		// getInput(){} --> user getter here to GET INPUT
+		console.log(App.arrPlayer);	//add timer function that forces uesr to enter strokes in set amount of time... at first...	
+
 	},
-	winOrLose: function(){
+	winOrLose(){
 		if (App.arrOutput === App.playerInput) {  //win/loose logic and messages.
-			console.log(`You are a winner!`)
+			console.log(`You managed to do ONE THING RIGHT... I guess...`)
 			App.counter ++;
 		} else {
-			console.log('You fucking suck at this game!  LOSER!')
+			console.log('You had ONE JOB!!! YOU BLEW IT!!!');
+
 		}
 	}
 };
 
-App.playGame();
 
 
 
-// we are pushing a bunch of random elements into an array 4 elements ramdomized into an array.
 
 
 
-// flex-flow: column-reverse;
-// flex-wrap: wrap-reverse;
-// justify-content: center;
-// align-content: space-between;
+
 
 
 
@@ -73,29 +72,48 @@ App.playGame();
 
 //listeners/handlers
 
+//add events for every key press:  when arrOutput receives new E light up btn
+//add events for every key press:  when arrInput receives new keypress light up btn
+
 document.addEventListener("keydown", (e) => {		//need to add animation logic for when keypress occurs here
 	if(['ArrowUp'].includes(event.key)){
-	console.log('redButton was pressed.')
+	console.log('redButton was pressed.');
+	App.arrPlayer.push(App.arrColorValues[0]);
+	console.log(App.arrPlayer);
 	}
 });
 
 document.addEventListener("keydown", (e) => {		//need to add animation logic for when keypress occurs here
 	if(['ArrowRight'].includes(event.key)){
-	console.log('yellowButton was pressed.')	
+	console.log('yellowButton was pressed.');
+	App.arrPlayer.push(App.arrColorValues[1]);
+	console.log(App.arrPlayer);
+
 	}
 });
 
 document.addEventListener("keydown", (e) => {		//need to add animation logic for when keypress occurs here
 	if(['ArrowDown'].includes(event.key)){
-	console.log('blueButton was pressed.')
+	console.log('blueButton was pressed.');
+	App.arrPlayer.push(App.arrColorValues[2]);
+	console.log(App.arrPlayer);
 	}
 });
 
 document.addEventListener("keydown", (e) => {		//need to add animation logic for when keypress occurs here
 	if(['ArrowLeft'].includes(event.key)){
-	console.log('greenButton was pressed.')	
+	console.log('greenButton was pressed.');
+	App.arrPlayer.push(App.arrColorValues[3]);
+	console.log(App.arrPlayer);
 	}
 });
+
+// $('#nameButton').on('click', () => {				//need this event listener to: start game @ submit clck
+													//display `${'#userNameBox.val()'`}name in top display <div>....
+// 	const userName = $('#userNameBox').val();
+// 	console.log(`${petName} IS ALIVE!! BE RESPONSIBLE!!`);
+// 	App.playGame();
+// });
 
 
 
